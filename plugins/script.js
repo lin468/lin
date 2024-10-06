9a80be949964cb29c6922132d5adb4e71329655c
lin <heather012384@gamil.com>
2024-10-06 15:38:09 +0800

    這是第一次提交

---
+++ plugins/script.js
@@ -0,0 +1,13 @@
+a = 100;
+b = 100;
+a+b;//200
+console.log('hello',a+b);
+console.log(a+b);
+console.error('hello');//紅色警告
+console.warn('hello');//黃色警告
+console.info('hello');
+ c='hello';
+ d='world';
+console.log('hello world');
+console.log(c+ ' ' +d);
+console.log(`${c} ${d}`);
